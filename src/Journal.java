12152d52fe9ad65d480a485c82f882fb729c5eea
Kirill Trezubov <kirilltrezubov@yandex.ru>
2019-06-13 00:11:04 +0300

    Journal. Добавлен метод insertOutParameters

--- src/Journal.java
+++ src/Journal.java
@@ -2,9 +2,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Journal extends Registration{
@@ -23,11 +21,13 @@ public class Journal extends Registration{
 
     public static void main(String[] args) {
         Journal storage_journal = new Journal();
-        storage_journal.insertInParameters(new Registration().getCurrentParametr("Fleur", "Weasley", "Isabella", "dbcash", "storage"),
-                storage_journal.getCurrentTimeStorage(new Date(),"dd.MM.yyyy hh:mm:ss"));
+//        storage_journal.insertInParameters(new Registration().getCurrentParametr("Fleur", "Weasley", "Isabella", "dbcash", "storage"),
+//                storage_journal.getCurrentTimeStorage(new Date(),"dd.MM.yyyy hh:mm:ss"));
+        storage_journal.insertOutParameters(new Registration().getCurrentParametr("Fleur", "Weasley", "Isabella", "dbcash", "storage"),
+                "13.06.2019 12:17:26", "27.02.2019 11:32:54");
     }
 
-//    Метод вставляет инофрмацию при заходе в ячейку (storage, start_time)
+//    Метод вставляет инофрмацию в журнал при заходе в ячейку (storage, start_time)
 
     private Boolean insertInParameters(int storage, String start_time) {
         String insertInData = "INSERT INTO STORAGE_JOURNAL"
@@ -36,10 +36,25 @@ public class Journal extends Registration{
 
         try {
             statement.executeUpdate(insertInData);
-            System.out.println("Data was inserted !");
+            System.out.println("Input data was inserted !");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-            System.out.println("Data wasn't insert to STORAGE_JOURNAL");
+            System.out.println("Input data wasn't insert to STORAGE_JOURNAL");
+            return false;
+        }
+        return true;
+    }
+
+    //    Метод вставляет инофрмацию в журнал при выходе из ячейки (storage, start_time)
+
+    private Boolean insertOutParameters(int Storage, String start_time, String end_time) {
+        String updateParametersInTable = String.format("UPDATE STORAGE_JOURNAL SET END_TIME = to_date('%s', 'dd.mm.yyyy hh:mi:ss') WHERE START_TIME = to_date('%s', 'dd.mm.yyyy hh:mi:ss')", end_time, start_time);
+        try {
+            statement.executeUpdate(updateParametersInTable);
+            System.out.println("Out data was inserted !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Out data wasn't insert to STORAGE_JOURNAL");
             return false;
         }
         return true;
