ec6e546cdf8e5df96d0a70d92175be94f7843a00
Kirill Trezubov <kirilltrezubov@yandex.ru>
2019-06-13 23:32:41 +0300

    Exchange. Создан метод changeGalleonsMoney для обмена галлеонов в любую валюту

--- src/Exchange.java
+++ src/Exchange.java
@@ -21,11 +21,12 @@ public class Exchange extends Storage {
 
     public static void main(String[] args) {
         Exchange exchange = new Exchange();
-        exchange.insertDataToExchangeCurrencySQL("rub", 1300280, 0.00435, 0.00439, 1120000, 0, 0);
-        exchange.insertDataToExchangeCurrencySQL("usd", 763800, 0.01512, 0.01712, 965350, 0, 0);
-        exchange.insertDataToExchangeCurrencySQL("gbp", 940000, 0.20312, 0.22712, 1400000, 0, 0);
-        exchange.insertDataToExchangeCurrencySQL("mag", 3750000, 4.93012, 4.93912, 2840000, 0, 0); //Относитльно фунта стерлингов
-        exchange.getExchangeCurrency();
+//        exchange.insertDataToExchangeCurrencySQL("rub", 1300280, 0.00435, 0.00439, 0, 0, 0);
+//        exchange.insertDataToExchangeCurrencySQL("usd", 763800, 0.01512, 0.01712, 0, 0, 0);
+//        exchange.insertDataToExchangeCurrencySQL("gbp", 940000, 0.20312, 0.22712, 0, 0, 0);
+//        exchange.insertDataToExchangeCurrencySQL("mag", 3750000, 4.93012, 4.93912, 0, 0, 0); //Относитльно фунта стерлингов
+//        exchange.getExchangeCurrency();
+        exchange.changeGalleonsMoney("usd", 10234);
     }
 
     //Метод для вставки данных в таблицу EXCHANGE_CURRENCY
@@ -50,12 +51,31 @@ public class Exchange extends Storage {
     //Метод делает обмен галлионов в любую валюту
 
     private Boolean changeGalleonsMoney(String to_money, int galleons) {
-        int current_sold = getMoneyParameter("mag", "sold") + galleons ;
-        int purchase_rate = getMoneyParameter(to_money, "purchase_rate");
-        int exchange_money = purchase_rate * galleons;
-        int current_bought = getMoneyParameter(to_money, "bought") + exchange_money;
 
-//        updateTable("exchange_currency", current_sold)
+        // увеличить покупку и продажу соответствующей валюты
+
+        double mag_new_sold = getMoneyParameter("mag", "sold").getArgument().doubleValue() + galleons;
+        double mag_bought = getMoneyParameter("mag", "bought").getArgument().doubleValue();
+
+        double purchase_rate = getMoneyParameter(to_money, "purchase_rate").getArgument().doubleValue();
+        double exchange_money = purchase_rate * galleons;
+
+        double to_money_new_bought = getMoneyParameter(to_money, "bought").getArgument().doubleValue() + exchange_money;
+        double to_money_sold = getMoneyParameter(to_money, "sold").getArgument().doubleValue();
+
+        updateTable("exchange_currency", "sold", new MultiArgument<Double>(mag_new_sold), "currency", new MultiArgument<String>("mag"));
+        updateTable("exchange_currency", "bought", new MultiArgument<Double>(to_money_new_bought), "currency", new MultiArgument<String>(to_money));
+
+        //обновить остаток
+
+        double mag_balance_beginning = getMoneyParameter("mag", "balance_beginning").getArgument().doubleValue();
+        double to_money_balance_beginning = getMoneyParameter(to_money, "balance_beginning").getArgument().doubleValue();
+
+        double new_mag_remainder = mag_balance_beginning + mag_bought - mag_new_sold;
+        double new_to_money_remainder = to_money_balance_beginning + to_money_new_bought - to_money_sold;
+
+        updateTable("exchange_currency", "remainder", new MultiArgument<Double>(new_mag_remainder), "currency", new MultiArgument<String>("mag"));
+        updateTable("exchange_currency", "remainder", new MultiArgument<Double>(new_to_money_remainder), "currency", new MultiArgument<String>(to_money));
 
 
 
@@ -86,20 +106,35 @@ public class Exchange extends Storage {
         return exchange_map;
     }
 
+// Класс generic для извлечения double и int инф. из строк таблицы
 
-//    Метод получает числовой параметр из таблцы EXCHANGE_TABLE
-
-    private Integer getMoneyParameter(String currency, String column) {
+    private NumberArguments getMoneyParameter(String currency, String column) {
         String selectFromExchangeCurrentSQL = String.format("SELECT %s FROM EXCHANGE_TABLE WHERE CURRENCY = '%s'", column.toUpperCase(), currency.toUpperCase());
-        Integer parameter = null;
+        NumberArguments parameter = null;
         try {
             ResultSet rs = statement.executeQuery(selectFromExchangeCurrentSQL);
             while (rs.next()) {
-                parameter = Integer.parseInt(rs.getString(column));
+                if (!rs.getString(column).contains(".")) {
+                    parameter = new NumberArguments<Integer>(Integer.parseInt(rs.getString(column)));
+                } else {
+                    parameter = new NumberArguments<Double>(Double.parseDouble(rs.getString(column)));
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return parameter;
+    }
+
+//    Метод получает числовой параметр из таблцы EXCHANGE_TABLE
+
+    private class NumberArguments<T extends Number> {
+        private T argument;
+        NumberArguments(T argument) {
+            this.argument = argument;
+        }
+        public T getArgument() {
+            return argument;
+        }
     }
 }
